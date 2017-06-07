15626db7dd2050750cad4dff9ef52954640363c6
Riccardo Righi <rrighi@holon-platform.com>
2017-06-07 11:15:19 +0200

    JPA datastore documentation

--- documentation/src/main/java/com/holonplatform/jpa/examples/ExampleJpaDatastoreSpring.java
+++ documentation/src/main/java/com/holonplatform/jpa/examples/ExampleJpaDatastoreSpring.java
@@ -20,6 +20,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -30,10 +31,10 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.holonplatform.core.datastore.Datastore;
+import com.holonplatform.datastore.jpa.JpaDatastore;
 import com.holonplatform.jdbc.spring.EnableDataSource;
 import com.holonplatform.jpa.spring.EnableJpa;
 import com.holonplatform.jpa.spring.EnableJpaDatastore;
-import com.holonplatform.jpa.spring.test.domain1.TestJpaDomain1;
 
 @SuppressWarnings("unused")
 public class ExampleJpaDatastoreSpring {
@@ -79,6 +80,59 @@ public class ExampleJpaDatastoreSpring {
 	// end::jpa[]
 
 	class Test {
+
+	}
+
+	// tag::multi[]
+	@Configuration
+	@PropertySource("jpa.properties")
+	@EnableTransactionManagement
+	class MultiJpaConfig {
+
+		@Configuration
+		@EnableJpa(dataContextId = "one", entityPackageClasses = Test1.class)
+		class ConfigOne {
+		}
+
+		@Configuration
+		@EnableJpa(dataContextId = "two", entityPackageClasses = Test2.class)
+		class ConfigTwo {
+		}
+
+	}
+
+	@Autowired
+	@Qualifier("one")
+	private DataSource dataSource1;
+	@Autowired
+	@Qualifier("one")
+	private EntityManagerFactory entityManagerFactory1;
+	@Autowired
+	@Qualifier("one")
+	private PlatformTransactionManager transactionManager1;
+	@Autowired
+	@Qualifier("one")
+	private JpaDatastore datastore1;
+
+	@Autowired
+	@Qualifier("two")
+	private DataSource dataSource2;
+	@Autowired
+	@Qualifier("two")
+	private EntityManagerFactory entityManagerFactory2;
+	@Autowired
+	@Qualifier("two")
+	private PlatformTransactionManager transactionManager2;
+	@Autowired
+	@Qualifier("two")
+	private JpaDatastore datastore2;
+	// end::multi[]
+
+	class Test1 {
+
+	}
+
+	class Test2 {
 
 	}
 
