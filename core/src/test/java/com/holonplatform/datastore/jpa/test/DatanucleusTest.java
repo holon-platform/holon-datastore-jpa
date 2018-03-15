4a4a87c069462efd32ce04ef0c3e5e739fba03a4
Riccardo Righi <rr9379@gmail.com>
2018-03-15 09:52:55 +0100

    Use a profile for Datanucleus unit tests

--- core/src/test/java/com/holonplatform/datastore/jpa/test/DatanucleusTest.java
+++ core/src/test/java/com/holonplatform/datastore/jpa/test/DatanucleusTest.java
@@ -27,32 +27,26 @@ import static com.holonplatform.datastore.jpa.test.model.TestDataModel.STR;
 import static com.holonplatform.datastore.jpa.test.model.TestDataModel.TMS;
 import static com.holonplatform.datastore.jpa.test.model.TestDataModel.VIRTUAL_STR;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.spi.PersistenceProvider;
-import javax.persistence.spi.PersistenceProviderResolverHolder;
+import javax.persistence.Persistence;
 
-import org.apache.openjpa.persistence.PersistenceProviderImpl;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
-import com.holonplatform.core.internal.utils.ConversionUtils;
 import com.holonplatform.core.property.PathProperty;
 import com.holonplatform.core.property.PropertySet;
-import com.holonplatform.core.property.PropertyValueConverter;
 import com.holonplatform.core.property.TemporalProperty;
 import com.holonplatform.datastore.jpa.JpaDatastore;
 import com.holonplatform.datastore.jpa.JpaTarget;
 import com.holonplatform.datastore.jpa.ORMPlatform;
 import com.holonplatform.datastore.jpa.test.config.DatastoreConfigCommodity;
 import com.holonplatform.datastore.jpa.test.expression.KeyIsFilter;
+import com.holonplatform.datastore.jpa.test.model.dentity.Test1;
+import com.holonplatform.datastore.jpa.test.model.dentity.Test3;
 import com.holonplatform.datastore.jpa.test.suite.AbstractJpaDatastoreTestSuite;
 import com.holonplatform.jdbc.DataSourceBuilder;
 
-public class ProfileTestOpenJpa extends AbstractJpaDatastoreTestSuite {
+public class DatanucleusTest extends AbstractJpaDatastoreTestSuite {
 
 	private static EntityManagerFactory entityManagerFactory;
 
@@ -60,59 +54,21 @@ public class ProfileTestOpenJpa extends AbstractJpaDatastoreTestSuite {
 	public static void initDatastore() {
 
 		// init db
-		DataSourceBuilder.builder().url("jdbc:h2:mem:datastore3;DB_CLOSE_ON_EXIT=FALSE").username("sa")
+		DataSourceBuilder.builder().url("jdbc:h2:mem:datastore4;DB_CLOSE_ON_EXIT=FALSE").username("sa")
 				.withInitScriptResource("h2/init.sql").build();
 
-		org.apache.openjpa.persistence.PersistenceProviderImpl openjpaProvider = null;
-		List<PersistenceProvider> pp = PersistenceProviderResolverHolder.getPersistenceProviderResolver()
-				.getPersistenceProviders();
-		for (PersistenceProvider p : pp) {
-			if (p instanceof org.apache.openjpa.persistence.PersistenceProviderImpl) {
-				openjpaProvider = (PersistenceProviderImpl) p;
-			}
-		}
-
-		if (openjpaProvider == null) {
-			throw new RuntimeException("Failed to load org.apache.openjpa.persistence.PersistenceProviderImpl");
-		}
-
-		entityManagerFactory = openjpaProvider.createEntityManagerFactory(null, "META-INF/persistence-openjpa.xml",
-				null);
+		entityManagerFactory = Persistence.createEntityManagerFactory("test_datanucleus");
 
 		datastore = JpaDatastore.builder().entityManagerFactory(entityManagerFactory).traceEnabled(true)
 				.withCommodity(DatastoreConfigCommodity.FACTORY).withExpressionResolver(KeyIsFilter.RESOLVER).build();
 
-		platform = ORMPlatform.OPENJPA;
-		
-		rightJoinTest = false;
-		blobArrayProjectionTest = false;
-		entityProjectionTest = false;
-		temporalPartFunctionTest = false;
-		temporalProjectionTest = false;
-		transactionalTest = false;
-		avgProjectionTest = false;
-		saveOperationTypeTest = false;
-		saveOperationTest = false;
-		updateNullsTest = false;
+		platform = ORMPlatform.DATANUCLEUS;
 
-		JPA_TARGET = JpaTarget.of(com.holonplatform.datastore.jpa.test.model.oentity.Test1.class);
+		JPA_TARGET = JpaTarget.of(Test1.class);
 
-		LDAT = TemporalProperty.localDate("localDateValue").converter(PropertyValueConverter.localDate());
-		LTMS = TemporalProperty.localDateTime("localDatetimeValue").converter(PropertyValueConverter.localDateTime());
-		TIME = TemporalProperty.localTime("localTimeValue").converter(Date.class, d -> ConversionUtils.toLocalTime(d),
-				t -> {
-					if (t == null)
-						return null;
-					Calendar c = Calendar.getInstance();
-					c.set(Calendar.YEAR, 1970);
-					c.set(Calendar.MONTH, 0);
-					c.set(Calendar.DAY_OF_MONTH, 1);
-					c.set(Calendar.HOUR_OF_DAY, t.getHour());
-					c.set(Calendar.MINUTE, t.getMinute());
-					c.set(Calendar.SECOND, t.getSecond());
-					c.set(Calendar.MILLISECOND, 0);
-					return c.getTime();
-				});
+		LDAT = TemporalProperty.localDate("localDateValue");
+		LTMS = TemporalProperty.localDateTime("localDatetimeValue");
+		TIME = TemporalProperty.localTime("localTimeValue");
 
 		PROPERTIES = PropertySet.builderOf(KEY, STR, DBL, DAT, LDAT, ENM, NBOOL, NST_STR, NST_DEC, TMS, LTMS, TIME)
 				.identifier(KEY).build();
@@ -123,10 +79,18 @@ public class ProfileTestOpenJpa extends AbstractJpaDatastoreTestSuite {
 
 		CLOB_SET_STR = PropertySet.of(PROPERTIES, CLOB_STR);
 
-		TEST3 = JpaTarget.of(com.holonplatform.datastore.jpa.test.model.oentity.Test3.class);
+		TEST3 = JpaTarget.of(Test3.class);
 
 		TEST3_CODE_P = PathProperty.create("pk.code", long.class).parent(TEST3);
 		TEST3_TEXT_P = PathProperty.create("text", String.class).parent(TEST3);
+
+		enumProjectionTest = false;
+		transactionalTest = false;
+		blobArrayProjectionTest = false;
+		updateWithFunctionTest = false;
+		updateNestedTest = false;
+		saveOperationTest = false;
+		entityProjectionTest = false;
 	}
 
 	@AfterClass
